07b65c6f414fb63996f8191e36e610d3e12b3942
jrcolby <j.r.colby@gmail.com>
2022-01-01 16:10:16 -0700

    dusting off the cobwebs

--- src/App.js
+++ src/App.js
@@ -1,79 +1,83 @@
 import "./App.css";
 import * as Tone from "tone";
 
+// Constants for possible values od randomized kick values, along with
+// probability distribution
 const oscillators = [
-  {
-    value: "triangle",
-    chance: 0.45,
-  },
-  {
-    value: "sine",
-    chance: 0.45,
-  },
-  {
-    value: "square",
-    chance: 0.1,
-  },
+	{
+		value: "triangle",
+		chance: 0.45,
+	},
+	{
+		value: "sine",
+		chance: 0.45,
+	},
+	{
+		value: "square",
+		chance: 0.1,
+	},
 ];
 
 const clickTypes = [
-  {
-    value: "C4",
-    chance: 0.5,
-  },
-  {
-    value: "C5",
-    chance: 0.5,
-  },
+	{
+		value: "C4",
+		chance: 0.25,
+	},
+	{
+		value: "C5",
+		chance: 0.25,
+	},
+	{
+		value: "D5",
+		chance: 0.25,
+	},
+	{
+		value: "E5",
+		chance: 0.25,
+	},
 ];
-
 const distortionLevel = [
-  {
-    value: 0,
-    chance: 0.35,
-  },
-  {
-    value: 0.1,
-    chance: 0.3,
-  },
-  {
-    value: 0.5,
-    chance: 0.2,
-  },
-  {
-    value: 1.3,
-    chance: 0.15,
-  },
+	{
+		value: 0,
+		chance: 0.5,
+	},
+	{
+		value: 0.1,
+		chance: 0.2,
+	},
+	{
+		value: 0.5,
+		chance: 0.15,
+	},
+	{
+		value: 1.3,
+		chance: 0.15,
+	},
 ];
 
-const bitcrushLevel = [
-  {
-    value: 0,
-    chance: 0.6,
-  },
-  {
-    value: 12,
-    chance: 0.3,
-  },
-  {
-    value: 8,
-    chance: 0.3,
-  },
-];
 
 /**
  * Make JSON object that contains all properties for synth,
  * sampler, and effects
  */
 let kickParams = makeKickParams();
-let membraneKick = generateSynth(kickParams);
-console.log(kickParams);
 
+// make boomy part of kick with a membrane synth generated from kickParams
+let membraneKick = generateSynth(kickParams);
+
+// init sampler with different kick clicks, release time and click volume
 let clickSampler = new Tone.Sampler({
-  urls: {
-    C4: "/woodblock.mp3",
-    C5: "/clave.mp3",
-  },
+	urls: {
+		C4: "woodblock.mp3",
+		C5: "clave.mp3",
+		D5: "click.mp3",
+		E5: "hihat.mp3"
+	},
+	onload: () => { console.log("loaded"); }
+});
+
+clickSampler.set({
+	release: 0.01,
 });
 
 let clickVol = new Tone.Volume(0 - getRandomBounded(20, 60));
@@ -82,13 +86,14 @@ let clickVol = new Tone.Volume(0 - getRandomBounded(20, 60));
 let loPass = generateFilter(kickParams);
 
 let hiPass = new Tone.Filter({
-  type: "highpass",
-  frequency: 20,
-  rolloff: -24,
+	type: "highpass",
+	frequency: 20,
+	rolloff: -24,
 });
 
 let distort = new Tone.Distortion(kickParams.distortLevel);
-let crusher = new Tone.BitCrusher(kickParams.bitcrushLevel);
+
+// Routing for master channel to main out (Tone.Destination)
 let limiter = new Tone.Limiter(0);
 let master = new Tone.Channel(0, 0);
 membraneKick.chain(distort, master);
@@ -96,102 +101,101 @@ clickSampler.chain(clickVol, master);
 master.chain(hiPass, loPass, limiter, Tone.Destination);
 
 function App() {
-  // Creating loop to sync sampler and membrane synth
-  let loop = new Tone.Loop(loopStep, "4n");
+	function makeKick() {
+		Tone.start();
+		Tone.Transport.stop();
+		kickParams = makeKickParams();
+		console.log(kickParams);
+		membraneKick = generateSynth(kickParams);
+		loPass = generateFilter(kickParams);
+		distort = new Tone.Distortion(kickParams.distortLevel);
+		membraneKick.chain(distort, master);
+		clickVol = new Tone.Volume(kickParams.clickVol);
+		console.log(clickVol);
+		clickSampler.chain(clickVol, master);
+		playKick();
+	}
 
-  function loopStep(time) {
-    clickSampler.triggerAttackRelease(kickParams.clickSample, 0.001, time);
-    console.log("GO");
-    membraneKick.triggerAttackRelease(kickParams.frequency, "4n", time);
-  }
 
-  function makeKick() {
-    Tone.Transport.stop();
-    kickParams = makeKickParams();
-    membraneKick = generateSynth(kickParams);
-    loPass = generateFilter(kickParams);
-    distort = new Tone.Distortion(kickParams.distortLevel);
-    crusher = new Tone.BitCrusher(kickParams.bitcrushLevel);
-    membraneKick.chain(distort, master);
-    clickSampler.chain(clickVol, master);
+	function playKick() {
 
-    // make a single loop step that plays once.
-  }
+		clickSampler.triggerAttackRelease(kickParams.clickSample, 0.001);
+		membraneKick.triggerAttackRelease(kickParams.frequency, "4n");
 
-  function playKick() {
-    Tone.Transport.start();
-    loop.start();
-  }
+	}
 
-  return (
-    <div className="App">
-      <button id="playKick" onClick={playKick}>
-        Play Kick
-      </button>
-      <button id="makeKick" onClick={makeKick}>
-        Make Kick
-      </button>
-    </div>
-  );
+	return (
+		<div className="App">
+			<button id="playKick" onClick={playKick}>
+				Play Kick
+			</button>
+			<button id="makeKick" onClick={makeKick}>
+				Make Kick
+			</button>
+			<audio controls></audio>
+		</div>
+	);
 }
 
 export default App;
 
+const audio = document.querySelector("audio");
+console.log(audio);
+
 function makeKickParams() {
-  const params = {
-    frequency: getRandomBounded(60, 100),
-    octaves: getRandomBounded(1, 5),
-    attack: 0,
-    decay: getRandomBounded(0, 0.5),
-    sustain: 0,
-    pitchDecay: getRandomBounded(0, 0.3),
-    waveform: getWeightedValue(oscillators),
-    filterFreq: getRandomBounded(100, 5000),
-    filterRolloff: -12,
-    filterType: "lowpass",
-    clickSample: getWeightedValue(clickTypes),
-    clickVol: getRandomBounded(12, 60),
-    distortLevel: getWeightedValue(distortionLevel),
-    crusherLevel: getWeightedValue(bitcrushLevel),
-  };
-  return params;
+	const params = {
+		frequency: getRandomBounded(60, 100),
+		octaves: getRandomBounded(1, 3),
+		attack: 0,
+		decay: getRandomBounded(0.15, 0.35),
+		sustain: 0,
+		pitchDecay: getRandomBounded(0, 0.3),
+		waveform: getWeightedValue(oscillators),
+		filterFreq: getRandomBounded(100, 5000),
+		filterRolloff: -12,
+		filterType: "lowpass",
+		clickSample: getWeightedValue(clickTypes),
+		clickVol: 0 - getRandomBounded(40, 60),
+		distortLevel: getWeightedValue(distortionLevel),
+	};
+	return params;
 }
 
 function generateFilter(kickParams) {
-  return new Tone.Filter({
-    type: kickParams.filterType,
-    frequency: kickParams.filterFreq,
-    rolloff: kickParams.filterRolloff,
-  });
+	return new Tone.Filter({
+		type: kickParams.filterType,
+		frequency: kickParams.filterFreq,
+		rolloff: kickParams.filterRolloff,
+	});
 }
 
 function getWeightedValue(objects) {
-  let randomNum = Math.random();
+	let randomNum = Math.random();
 
-  for (var i = 0; i < objects.length; i++) {
-    let obj = objects[i];
-    if (randomNum < obj.chance) {
-      return obj.value;
-    }
-    randomNum -= obj.chance;
-  }
+	for (var i = 0; i < objects.length; i++) {
+		let obj = objects[i];
+		if (randomNum < obj.chance) {
+			return obj.value;
+		}
+		randomNum -= obj.chance;
+	}
 }
 
 function getRandomBounded(min, max) {
-  return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
 }
 
 function generateSynth(kickParams) {
-  return new Tone.MembraneSynth({
-    octaves: kickParams.octaves,
-    envelope: {
-      attack: kickParams.attack,
-      decay: kickParams.decay,
-      sustain: kickParams.sustain,
-    },
-    pitchDecay: kickParams.pitchDecay,
-    oscillator: {
-      type: kickParams.waveform,
-    },
-  });
+	return new Tone.MembraneSynth({
+		octaves: kickParams.octaves,
+		envelope: {
+			attack: kickParams.attack,
+			decay: kickParams.decay,
+			sustain: kickParams.sustain,
+		},
+		pitchDecay: kickParams.pitchDecay,
+		oscillator: {
+			type: kickParams.waveform,
+		},
+	});
 }
